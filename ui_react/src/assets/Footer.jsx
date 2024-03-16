840602efdf1093b8729e36a1f3e04e7963a91a40
harshan <harshansreepm@gmail.com>
2024-03-16 09:41:18 +0530

    new

---
+++ ui_react/src/assets/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 fixed bottom-0 w-full">
+    <p>&copy; 2024 Your Website</p>
+  </footer>
+  )
+}
+
+export default Footer
---
+++ ui_react/src/assets/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Navbar'
+
+const Home = () => {
+  return (
+    <div> <Navbar />
+    <div className="text-yellow-500">
+      Home
+    </div>
+
+    </div>
+  )
+}
+
+export default Home
