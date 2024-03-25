9cf115cee736f8b206dd9ddfb7fd049045305207
harshan <harshansreepm@gmail.com>
2024-03-25 21:41:23 +0530

    sw

--- backend_spring/yoga/src/main/java/com/example/yoga/config/SecurityConfiguration.java
+++ backend_spring/yoga/src/main/java/com/example/yoga/config/SecurityConfiguration.java
@@ -14,8 +14,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.example.yoga.constant.Api;
-
-
 import lombok.RequiredArgsConstructor;
 
 @Configuration
